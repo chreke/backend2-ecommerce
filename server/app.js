fec0c24b58bebee2376a12ce322e382dd0b1c41b
panosjapan7 <panosjapan@gmail.com>
2022-03-28 11:23:18 +0200

    Add product routes

--- server/app.js
+++ server/app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 
+const { productRoutes} = require("./controllers/products.js");
+
 const app = express()
 const PORT = 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
+
+// /products
+// /products/ABC
+
+app.use("/products", productRoutes);
 
 app.listen(PORT, () => {
   console.log(`Started Express server on port ${PORT}`);
---
+++ server/controllers/products.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const { readProducts } = require("../models/products.js");
+
+const productRoutes = express.Router();
+
+productRoutes.get("/", async (req, res) => {
+    const products = await readProducts();
+    res.json(products);
+})
+
+productRoutes.get("/:sku", async (req, res) => {
+    const { products } = await readProducts();
+    const product = products.find(({sku}) => sku === req.params.sku );
+    
+    if(product) {
+        res.json(product);
+    }
+    else {
+        res.sendStatus(404);
+    }
+})
+
+module.exports = { productRoutes };
