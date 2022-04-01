162fbfcbfe7ad9b0b0319d990dfa2ffefe7915df
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-04-01 15:16:53 +0200

    Add test for making a POST to /products

--- server/controllers/products.test.js
+++ server/controllers/products.test.js
@@ -11,3 +11,19 @@ test("GET /products should list products", async () => {
   expect(response.headers["content-type"]).toMatch("application/json");
   expect(response.status).toBe(200);
 });
+
+test("POST /products should create a new product", async () => {
+  const product = {
+    name: "A product",
+    sku: "ABC-123",
+    price: 599,
+    description: "A test product",
+    thumbnail: "/foo-thumbnail.jpg",
+    image: "/foo.jpg"
+  };
+  const createdResponse = await request.post("/products").send(product);
+  expect(createdResponse.status).toBe(200);
+  const getResponse = await request.get("/products/ABC-123");
+  expect(getResponse.status).toBe(200);
+  expect(getResponse.body.name).toBe(product.name);
+});
