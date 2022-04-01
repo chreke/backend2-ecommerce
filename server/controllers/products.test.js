d9f7696f66dca579137e992e488ac25f3a1ba3a0
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-04-01 15:04:33 +0200

    Move database setup code to separate function

--- server/controllers/products.test.js
+++ server/controllers/products.test.js
@@ -1,21 +1,13 @@
 const supertest = require("supertest");
-const mongoose = require("mongoose");
 
+const { setupDatabase } = require("../common/test-utils");
 const app = require("../app");
 const request = supertest(app);
 
-const MONGODB_TEST_URL = "mongodb://127.0.0.1/test-ecommerce";
-
-beforeAll(async () => {
-  await mongoose.connect(MONGODB_TEST_URL);
-});
+setupDatabase();
 
 test("GET /products should list products", async () => {
   const response = await request.get("/products");
   expect(response.headers["content-type"]).toMatch("application/json");
   expect(response.status).toBe(200);
-});
-
-afterAll(async () => {
-  await mongoose.connection.close();
 });
--- server/models/products.test.js
+++ server/models/products.test.js
@@ -1,22 +1,8 @@
-const mongoose = require("mongoose");
+const { setupDatabase } = require("../common/test-utils");
 
 const { createProduct, getProduct } = require("./products");
 
-const MONGODB_TEST_URL = "mongodb://127.0.0.1/test-ecommerce";
-
-beforeAll(async () => {
-  await mongoose.connect(MONGODB_TEST_URL);
-});
-
-afterEach(async () => {
-  for (const collection of Object.values(mongoose.connection.collections)) {
-    await collection.deleteMany();
-  }
-});
-
-afterAll(async () => {
-  await mongoose.connection.close();
-});
+setupDatabase();
 
 test("should create product", async () => {
   const product = await createProduct({
