56de071270e10eecbb92e47025c90ad2612eeeed
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-04-03 00:15:12 +0200

    Add routes for user authentication

--- server/app.js
+++ server/app.js
@@ -3,11 +3,13 @@ const cors = require("cors");
 
 const productRoutes = require("./controllers/products");
 const userRoutes = require("./controllers/users");
+const { loginUser } = require("./controllers/auth");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(loginUser);
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,12 +1,29 @@
 const express = require("express");
 
 const { createUser, login } = require("../models/users")
+const { generateToken, requireLogin } = require("./auth");
 
 const userRoutes = express.Router();
 
 userRoutes.post("/users", async (req, res) => {
   const user = await createUser(req.body);
   return res.json({username: user.username});
+});
+
+userRoutes.post("/tokens", async (req, res) => {
+  const {username, password} = req.body;
+  const user = await login(username, password);
+  if (user) {
+    const token = generateToken(user);
+    res.json({token});
+  } else {
+    res.sendStatus(401);
+  }
+});
+
+userRoutes.get("/me", requireLogin, async (req, res) => {
+  const { username, email } = req.user;
+  res.json({ username, email });
 });
 
 module.exports = userRoutes ;
